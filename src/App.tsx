21ff9127258b6403f3fa5ef3d1a0437cf0bdb066
Yung Ju Rick Kim <yungjurick@jaranda.kr>
2021-02-24 01:27:22 +0900

    Add React Router and Gnb Component Layouts with navigation to core gnb routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,62 @@
 import styled from 'styled-components';
 import {Provider} from 'react-redux';
 import store from './common/store';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Category from './shared/Category/Category';
+import { GNBMainComponent } from './components/Gnb-component/GnbMain';
 
 const Root = styled.div`
   position: relative;
   width:360px;
   height:100vh;
   background: #FAFAFA;
+  @media (max-width: 380px) {
+    width: 100vw;
+  }
+  `
+const HeaderContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 56px;
+  `
+const AppContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 113px);
+  overflow-y: auto;
   `
 
 function App(){
   return (
     <Root>
       <Provider store={store}>
+        <Router>
+          {/* TOP HEADER CONTAINER */}
+          <HeaderContainer>
+            
+          </HeaderContainer>
+          {/* APP CONTAINER */}
+          <AppContainer>
+            <Switch>
+              <Route exact path="/">
+              </Route>
+              <Route path="/brand">
+              </Route>
+              <Route path="/category">
+                <Category/>
+              </Route>
+              <Route path="/community">
+              </Route>
+            </Switch>
+          </AppContainer>
+          {/* BOTTOM GNB CONTAINER */}
+          <GNBMainComponent />
+        </Router>
       </Provider>
     </Root>
   );
---
+++ src/components/Gnb-component/GnbItem.tsx
@@ -0,0 +1,23 @@
+import { useHistory, useLocation } from 'react-router-dom';
+import store from '../../common/store';
+import { IGnb, GNB_ITEM_TYPE, setActiveGnb } from './state';
+import { GNBItem, GNBLabel } from './style';
+
+export const GNBItemComponent = ({item}: {item: IGnb}) => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+
+  const onClickItem = (id: GNB_ITEM_TYPE,link: string, pathname: string) => {
+    if (link !== pathname) {
+      history.push(item.link)
+      store.dispatch(setActiveGnb(id));
+    }
+  }
+
+  return (
+    <GNBItem onClick={() => onClickItem(item.id, item.link, pathname)}>
+      <img src={item.selected ? item.icon[1] : item.icon[0]} alt="gnb-icon"/>
+      <GNBLabel selected={item.selected}>{item.label}</GNBLabel>
+    </GNBItem>
+  )
+}
